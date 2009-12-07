52591521ce529d34f6f99ea8947ff9729c6b7918
Adam Bielinski <windsurfer619@gmail.com>
2009-12-06 22:23:15 -0500

    ASCII works

--- Multicast_test/Controller.cs
+++ Multicast_test/Controller.cs
@@ -143,7 +143,7 @@ namespace Multicast_test
 					for (int i = 0; i < file_name.Length; i++){
 						file_name[i] = b[i];
 					}
-					new_file.file_name = BitConverter.ToString(file_name);
+					new_file.file_name = Encoding.ASCII.GetString(file_name);
 					
 					byte[] file_size = new byte[8];
 					for (int i = file_name.Length; i < file_name.Length+8; i++){
